dfd4c497d18618a55322544c4676736ce4bf653b
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-13 18:37:14 +0800

    export thunk and dispatch thunk function for 1st render

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,4 +1,14 @@
+import { useEffect  } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "../store";
+
 function UserList() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
   return(
     <div>
       UserList
