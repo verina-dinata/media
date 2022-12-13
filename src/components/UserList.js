5785eeeae532b51f3e2c34e57fc5dbea9584eea5
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-13 20:05:10 +0800

    add skeleton loader

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,7 +1,7 @@
 import { useEffect  } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
-
+import Skeleton from "./Skeleton";
 
 function UserList() {
   const dispatch = useDispatch();
@@ -15,7 +15,7 @@ function UserList() {
   }, [dispatch]);
 
   if (isLoading) {
-    return <div>Loading...</div>
+    return <Skeleton times={6} className='h-10 w-full' />
   }
 
   if (error) {
