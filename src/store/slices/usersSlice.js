0f60ce3f656e442990b933c42c889a0d092f6bc5
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-13 23:31:59 +0800

    add delete user feature

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -40,7 +40,9 @@ const usersSlice = createSlice({
     });
     builder.addCase(removeUser.fulfilled, (state, action) => {
       state.isLoading = false;
-      // fix me
+      state.data = state.data.filter(user => {
+        return user.id !== action.payload.id
+      })
       console.log(action);
     });
     builder.addCase(removeUser.rejected, (state, action) => {
