97b4b6857f62b69c874cff5bdff965c3b82f04b1
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-13 22:07:51 +0800

    add useState to components for loadingUsers and loadingUsersError

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,34 +1,44 @@
-import { useEffect  } from "react";
+import { useEffect, useState  } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import Button from './Button';
 import Skeleton from "./Skeleton";
 
 function UserList() {
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  const [loadingUsersError, setLoadingUsersError] = useState(null);
   const dispatch = useDispatch();
 
-  const {isLoading, data, error} = useSelector((state) => {
+  const { data } = useSelector((state) => {
     return state.users; // { data: [], isLoading: false. error: null }
   });
 
   useEffect(() => {
-    dispatch(fetchUsers());
+    setIsLoadingUsers(true);
+    dispatch(fetchUsers())
+      .unwrap()
+      .then(() => {
+        console.log('success');
+      })
+      .catch(() => {
+        console.log('FAIL');
+      });
   }, [dispatch]);
 
   const handleUserAdd = () => {
     dispatch(addUser());
   }
 
-  if (isLoading) {
+  if (isLoadingUsers) {
     return <Skeleton times={6} className='h-10 w-full' />
   }
 
-  if (error) {
+  if (loadingUsersError) {
     return <div>Error fetching data...</div>
   }
 
   const renderedUsers = data.map((user) => {
-    return <div key={user.id} className='mb-2 boorder rounded'>
+    return <div key={user.id} className='mb-2 border rounded'>
       <div className="flex p-2 justify-between items-center cursor-pointer">
         {user.name}
       </div>
