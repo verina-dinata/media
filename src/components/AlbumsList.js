fbba9f81fdce79986a0d338f340ab3d3c36b1a72
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-14 20:21:54 +0800

    add expandable feature for album

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -6,9 +6,24 @@ import Button from './Button';
 function AlbumsList({ user }) {
   const { data, error, isLoading } = useFetchAlbumsQuery(user); // when this is called, it will fetch data
 
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} />
+  } else if (error) {
+    content = <div>Error loading albums.</div>
+  } else {
+    content = data.map(album => {
+      const header = <div>{album.title}</div>
+      return <ExpandablePanel key={album.id} header={header}>
+        List of photos in album
+      </ExpandablePanel>
+    });
+  }
+
   return(
     <div>
-      Album for {user.name}
+      <div>Album for {user.name}</div>
+      <div>{content}</div>
     </div>
   );
 };
