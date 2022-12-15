852d76c286062f53e60ae6b2b378b57695a11422
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-15 18:35:43 +0800

    add add photo feature

--- src/components/PhotosList.js
+++ src/components/PhotosList.js
@@ -1,9 +1,23 @@
-import { useFetchPhotosQuery } from "../store";
+import { useFetchPhotosQuery, useAddPhotoMutation, useRemovePhotoMutation } from "../store";
+import Button from './Button';
 
 function PhotosList({ album }) {
   useFetchPhotosQuery(album);
+  const [addPhoto, addPhotoResults] = useAddPhotoMutation();
+
+  const handleAddPhoto = () => {
+    addPhoto(album);
+  }
+
   return(
-    <div>PhotosList</div>
+    <div>
+      <div className="m-2 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">Photos in {album.title}</h3>
+        <Button loading={addPhotoResults.isLoading} onClick={handleAddPhoto}>
+          + Add Photo
+        </Button>
+      </div>
+    </div>
   );
 };
 
